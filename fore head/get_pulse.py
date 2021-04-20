3a770850559c096c45335d39cb0009f71e15868f
Janarthanans07 <janarthanan07012000@gmail.com>
2021-04-20 11:54:30 +0530

    initial commit

---
+++ fore head/get_pulse.py
@@ -0,0 +1,123 @@
+from lib.device import Camera
+from lib.processors_noopenmdao import findFaceGetPulse
+from lib.interface import plotXY, imshow, waitKey, destroyWindow
+from cv2 import moveWindow
+import argparse
+import numpy as np
+import datetime
+#TODO: work on serial port comms, if anyone asks for it
+#from serial import Serial
+import socket
+import sys
+
+class getPulseApp(object):
+
+    """
+    Python application that finds a face in a webcam stream, then isolates the
+    forehead.
+
+    Then the average green-light intensity in the forehead region is gathered
+    over time, and the detected person's pulse is estimated.
+    """
+
+    def __init__(self, args):
+        # Imaging device - must be a connected camera (not an ip camera or mjpeg
+        # stream)
+        serial = args.serial
+        baud = args.baud
+        self.send_serial = False
+        if serial:
+            self.send_serial = True
+            if not baud:
+                baud = 9600
+            else:
+                baud = int(baud)
+            self.serial = Serial(port=serial, baudrate=baud)
+
+        
+
+        self.cameras = []
+        self.selected_cam = 0
+        for i in range(3):
+            camera = Camera(camera=i)  # first camera by default
+            if camera.valid or not len(self.cameras):
+                self.cameras.append(camera)
+            else:
+                break
+        self.w, self.h = 0, 0
+        self.pressed = 0
+        # Containerized analysis of recieved image frames (an openMDAO assembly)
+        # is defined next.
+
+        # This assembly is designed to handle all image & signal analysis,
+        # such as face detection, forehead isolation, time series collection,
+        # heart-beat detection, etc.
+
+        # Basically, everything that isn't communication
+        # to the camera device or part of the GUI
+        self.processor = findFaceGetPulse(bpm_limits=[50, 160],
+                                          data_spike_limit=2500.,
+                                          face_detector_smoothness=10.)
+
+        self.key_controls = {}
+
+    
+
+    
+    def key_handler(self):
+        """
+        Handle keystrokes, as set at the bottom of __init__()
+
+        A plotting or camera frame window must have focus for keypresses to be
+        detected.
+        """
+
+        self.pressed = waitKey(10) & 255  # wait for keypress for 10 ms
+        if self.pressed == 27:  # exit program on 'esc'
+            print("Exiting")
+            for cam in self.cameras:
+                cam.cam.release()
+            if self.send_serial:
+                self.serial.close()
+            sys.exit()
+
+        for key in self.key_controls.keys():
+            if chr(self.pressed) == key:
+                self.key_controls[key]()
+
+    def main_loop(self):
+        """
+        Single iteration of the application's main loop.
+        """
+        # Get current image frame from the camera
+        frame = self.cameras[self.selected_cam].get_frame()
+        self.h, self.w, _c = frame.shape
+
+        # display unaltered frame
+        # imshow("Original",frame)
+
+        # set current image frame to the processor's input
+        self.processor.frame_in = frame
+        # process the image frame to perform all needed analysis
+        self.processor.run(self.selected_cam)
+        # collect the output frame for display
+        output_frame = self.processor.frame_out
+
+        # show the processed/annotated output frame
+        imshow("Processed", output_frame)
+
+
+        # handle any key presses
+        self.key_handler()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Webcam pulse detector.')
+    parser.add_argument('--serial', default=None,
+                        help='serial port destination for bpm data')
+    parser.add_argument('--baud', default=None,
+                        help='Baud rate for serial transmission')
+
+    args = parser.parse_args()
+    App = getPulseApp(args)
+    while True:
+        App.main_loop()
